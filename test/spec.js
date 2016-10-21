967e15ea1bbd927d3996434b269d0e7dcb7a236c
Kevin Isom <kevin.isom@gmail.com>
2016-10-22 09:59:37 +1300

    neighbor count

--- test/spec.js
+++ test/spec.js
@@ -4,16 +4,26 @@ describe('Conways Game of Life', () => {
   it('Any live cell with fewer than two live neighbours dies', (done) => {
     const board = {
       grid: [[0, 0, 0], [0, 1, 0], [0, 0, 1]],
-      checkCell: (x, y) => {
-
+      checkCell: function (x, y) {
+        console.log(this);
+        return this.grid[x][y] === 1;
       },
-      turn: () => {
+      turn: function () {
+        for (let rowIndex = 0; rowIndex <= 3; rowIndex++) {
+          for (let columnIndex = 0; columnIndex <=3; columnIndex++) {
+            const currentCell = { x: rowIndex, y: columnIndex };
+          }
+        }
+      },
+      getNeighborCount: function({ x, y }) {
 
       }
     };
 
     assert.ok(board.checkCell(1, 1));
-
+    //board.turn();
+    assert.ok(board.getNeighborCount({x: 1, y: 1}) === 1);
+    //assert.ok(board.checkCell(1, 1) === false);
     done();
   });
 });
