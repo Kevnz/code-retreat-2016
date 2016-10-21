623c312d6c62b7e28c6c20480f75139f7adee5e5
Kevin Isom <kevin.isom@gmail.com>
2016-10-22 09:45:50 +1300

    first

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,19 @@
 const assert = require('assert');
 
-describe('what?', () => {
-  it('should do what...', (done) => {
-    assert.ok(false);
+describe('Conways Game of Life', () => {
+  it('Any live cell with fewer than two live neighbours dies', (done) => {
+    const board = {
+      grid: [[0, 0, 0], [0, 1, 0], [0, 0, 1]],
+      checkCell: (x, y) => {
+
+      },
+      turn: () => {
+
+      }
+    };
+
+    assert.ok(board.checkCell(1, 1));
+
     done();
   });
 });
